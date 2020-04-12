57c2ed391baf32e9bdeeafbcdf4949f3b7f10b49
yurtsevichK <nicecurls@icloud.com>
2020-04-12 07:31:07 +0300

    Task day3 class Person

---
+++ src/by/it/yurtsevich/homeTasks/homeTask_day3/Person.java
@@ -0,0 +1,10 @@
+package by.it.yurtsevich.homeTasks.homeTask_day3;
+
+public class Person extends Human {
+    public String name;
+    public String surname;
+
+    public Person(int age) {
+        super(age);
+    }
+}
