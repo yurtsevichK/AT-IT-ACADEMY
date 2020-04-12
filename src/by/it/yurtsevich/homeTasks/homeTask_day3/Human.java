f18a0f2a408462ae5953f460c8e41c972eedc586
yurtsevichK <nicecurls@icloud.com>
2020-04-12 07:31:26 +0300

    Task day3 class Human

---
+++ src/by/it/yurtsevich/homeTasks/homeTask_day3/Human.java
@@ -0,0 +1,14 @@
+package by.it.yurtsevich.homeTasks.homeTask_day3;
+
+class Human {
+    public int age;
+
+    public Human(int age) {
+        this.age = age;
+    }
+
+    public void speakAble() {
+        System.out.println("Говорить");
+    }
+}
+
